a6199ef37d259ffa9ad72d0d0f8a8f8f7a03e372
Ben Plumley <benplumley@gmail.com>
2015-02-27 23:48:06 +0000

    Add fields to Contact

--- Contact.java
+++ Contact.java
@@ -2,6 +2,11 @@ public class Contact {
 
 	// this class will be instantiated to create a unique contact in the address book.
 
+	private String firstName;
+	private String surname;
+	private String mobileNumber; // phone numbers must be strings to preserve leading zeroes
+	private String homeNumber;
+
 	public Contact() {
 
 	}
