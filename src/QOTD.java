be9ba7e45963bfdd816edfb3c43f2a500fbca4f8
Ben Plumley <benplumley@gmail.com>
2015-03-24 11:05:08 +0000

    Comment and method defs

--- src/QOTD.java
+++ src/QOTD.java
@@ -1,7 +1,13 @@
 public class QOTD {
 
+	// quotes should be read from Data/Quotes/quotes.txt and written to Data/Quotes/recent.txt
+
 	public QOTD() {
-		
+
+	}
+
+	private String[] loadQuoteFromFile() {
+
 	}
 
 }
