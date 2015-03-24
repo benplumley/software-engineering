8f28573f253807a60b1dd33cf3ea4e9e64e73144
Benn <bennairlie@gmail.com>
2015-03-24 11:48:56 +0000

    Finished Tasks

--- src/Task.java
+++ src/Task.java
@@ -1,15 +1,61 @@
 import java.io.File;
-
+import java.util.UUID;
 public class Task {
+	private final UUID id;
+	private String taskName;
+	private Date dueDateTime;
+	private boolean taskStatus;
+	private String taskNotes;
 
 	// this class will be instantiated to create a unique coursework/homework task.
 
 	public Task(File saveFile) {
 
+
 	}
 
-	public Task(String uniqueID) {
+	public Task(UUID uniqueID, String name, Date due,String notes) {
+		id = uniqueID;
+		taskName = name;
+		dueDateTime = due;
+		taskStatus = false;
+		taskNotes = notes;
+	}
 
+	public UUID getID() {
+		return id;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public Date getDueDateTime() {
+		return dueDateTime;
+	}
+
+	public boolean getTaskStatus() {
+		return status;
+	}
+
+	public String getTaskNotes() {
+		return taskNotes;
+	}
+
+	public void setTaskName(String newName) {
+		taskName = newName;
+	}
+
+	public void setDueDateTime(Date newDueDate) {
+		dueDateTime = newDueDate;
+	}
+
+	public void setTaskStatus(boolean newStatus) {
+		taskStatus = newStatus;
+	}
+
+	public void setTaskNotes(String newNotes) {
+		taskNotes = newNotes;
 	}
 
 }
