980405f46eef3f53ca9907504da8160403450198
bairlie <bennairlie@gmail.com>
2015-04-14 10:55:28 +0100

    Updated EventManager

--- src/EventManager.java
+++ src/EventManager.java
@@ -1,47 +1,27 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.*;
+import java.util.Map;
+import java.util.UUID;
+import java.util.HashMap;
 
 public class EventManager extends Manager {
 
-	private ArrayList<Event> eventList = new ArrayList<Event>();
+	private Map<UUID, Event> events;
 	private static final int SAVE_FILE_LENGTH = 10;
 
 	public EventManager() {
-		loadEventsFromFile();
+		events = new HashMap<>();
+		loadevents();
 	}
 
-	private void loadEventsFromFile() {
-		File[] saveFiles = new File("/Data/My Events").listFiles();
-		for (File saveFile : saveFiles) { // iterates through the files in the directory
-			if (isFileValid(saveFile)) {
-				eventList.add(new Event(saveFile));
-			}
+	protected void loadEvents() {
+		for (List<String> lines : loadFiles("/Data/My Events")) {
+			Event event = new Event(lines.get(0), lines);
 		}
 	}
 
-	private boolean isFileValid(File toCheck) {
-		ArrayList<String> lines = new ArrayList<String>();
-		try (Scanner fileReader = new Scanner(toCheck)) {
-			int i = 0;
-			while (fileReader.hasNext()) {
-				lines.add(new String(fileReader.nextLine()));
-			}
-			if (lines.size() != SAVE_FILE_LENGTH) { // file did not contain the right number of lines
-				return false;
-			} else if (lines.get(0).length() == 0) { // no GUID was provided
-				return false;
-			} else if (lines.get(1).length() == 0) { // no event name was provided
-				return false;
-			} else if (lines.get(2).length() == 0) { // no start date was provided
-				return false;
-			} else if (lines.get(3).length() == 0) { // no end date was provided
-				return false;
-			}
-			return true;
-		} catch (FileNotFoundException e) { // the file does not exist
-			return false;
-		}
+	@Override
+	protected boolean isFileValid(List<String> lines) {
+		return lines.size() == SAVE_FILE_LENGTH && lines.get(0).length() > 0 &&
+		 lines.get(1).length() > 0 && lines.get(2).length() > 0 && lines.get(3).length() > 0; // id, event name, start date and end date is required
 	}
 
 }
