1a42a23a3577ae549162a0b5c718d4db65d5aa84
bairlie <bennairlie@gmail.com>
2015-04-21 15:00:15 +0100

    Simple GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,9 +1,26 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+public class GUI {
 
-public class GUI extends JFrame {
+	public static void main(String[] args) {
+		initGUI();
+	}
 
-	public GUI() {
-
+	public static void initGUI() {
+		JFrame guiFrame = new JFrame("PIM");
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setSize(142,198);
+		guiFrame.setVisible(true);
+		JPanel homePanel = new JPanel();
+		homePanel.setLayout(new BoxLayout(homePanel, BoxLayout.PAGE_AXIS));
+		JButton contactButton = new JButton("Contacts");
+		JButton taskButton = new JButton("Tasks");
+		homePanel.add(contactButton);
+		contactButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		homePanel.add(taskButton);
+		taskButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		guiFrame.add(homePanel);
+		homePanel.setVisible(true);
 	}
 
 }
